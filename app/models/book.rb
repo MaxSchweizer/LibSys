ece69dc7250d79939c48fba6b3599077d44b6da4
Max Schweizer <meschwei@ncsu.edu>
2015-09-12 17:36:46 -0400

    Created book controller and index method

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,13 @@
 class Book < ActiveRecord::Base
   has_many :histories
   has_many :library_members, through: :histories
+
+  def checked_out?
+    histories.each do |history|
+      if history.return.nil?
+        return true
+      end
+    end
+    false
+  end
 end
