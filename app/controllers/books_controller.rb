815af5c3f2b88b04ac11b8ba6550c6f5b5d93c49
Daniel B <dabenavi@ncsu.edu>
2015-09-13 17:29:29 -0400

    validation of book entry added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,17 +8,26 @@ class BooksController < ApplicationController
     end
   end
 
+  def new
+    @book = Book.new
+  end
+
   def show
     @book = Book.find params[:id]
   end
+
   def create
     @book = Book.new message_params
 
-    @book.save
-    redirect_to @book
+    if @book.save
+      redirect_to @book
+    else
+      render 'new'
+    end
   end
 
   def message_params
     params.require(:book).permit(:isbn, :title, :author, :description)
   end
+
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,9 @@ class Book < ActiveRecord::Base
   has_many :histories
   has_many :library_members, through: :histories
 
+  validates :isbn, :title, :author, :description, presence: true
+  validates :isbn, uniqueness:  true
+
   def checked_out?
     histories.each do |history|
       if history.return.nil?
