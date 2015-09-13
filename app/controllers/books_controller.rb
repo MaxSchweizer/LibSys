49c3a7a76510a855b527195467081a2a69f1c9d4
Daniel B <dabenavi@ncsu.edu>
2015-09-13 16:55:04 -0400

    some views for books added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,4 +11,14 @@ class BooksController < ApplicationController
   def show
     @book = Book.find params[:id]
   end
+  def create
+    @book = Book.new message_params
+
+    @book.save
+    redirect_to @book
+  end
+
+  def message_params
+    params.require(:book).permit(:isbn, :title, :author, :description)
+  end
 end
