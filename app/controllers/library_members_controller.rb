3da498288bf839e0260be3375eeac836b98f936d
Max Schweizer <meschwei@ncsu.edu>
2015-09-13 16:27:13 -0400

    Started work on creating new library_members. Needs validation

--- app/controllers/library_members_controller.rb
+++ app/controllers/library_members_controller.rb
@@ -6,4 +6,18 @@ class LibraryMembersController < ApplicationController
   def show
     @library_member = LibraryMember.find params[:id]
   end
+
+  def new
+  end
+
+  def create
+    @library_member = LibraryMember.new message_params
+
+    @library_member.save
+    redirect_to @library_member
+  end
+
+  def message_params
+    params.require(:library_member).permit(:email, :name, :password)
+  end
 end
--- app/models/library_member.rb
+++ app/models/library_member.rb
@@ -1,4 +1,7 @@
 class LibraryMember < ActiveRecord::Base
   has_many :histories
   has_many :books, through: :histories
+
+  attr_accessor :password_confirmation
+  validates_confirmation_of :password
 end
