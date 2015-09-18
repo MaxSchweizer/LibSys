019aafc51d0e275657ea2ded849b509cb6edd021
Max Schweizer <meschwei@ncsu.edu>
2015-09-17 22:01:16 -0400

    Additional book controller testing

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,11 +1,55 @@
 require 'test_helper'
 
 class BooksControllerTest < ActionController::TestCase
-  # TODO: Write more tests for the controller...
 
+  # Test the index
   test "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns :books
+
+    assert (assigns(:books).length >= 10)
+    # TODO: Write more unit test for this.  Perhaps decrease the number of books in the fixture for comprehensive testing.
+  end
+
+  test "should be able to search by author with exact string" do
+    get :index, {"search_type" => "author", "query" => "Author0"}
+    assert_response :success
+    assert_not_nil assigns :books
+
+    assert_equal 1, assigns(:books).length
+    assert "ISBN0", assigns(:books)[0].isbn
+  end
+
+  test "should be able to search by author with weak match string" do
+    get :index, {"search_type" => "author", "query" => "Author"}
+    assert_response :success
+    assert_not_nil assigns :books
+
+    assert (assigns(:books).length >= 10)
+    # TODO: Write more unit test for this.  Perhaps decrease the number of books in the fixture for comprehensive testing.
+  end
+
+  # Test the create
+  test "create new book" do
+    flunk "Test not implemented yet"
+  end
+
+  test "create new book with bad data" do
+    flunk "Test not implemented yet"
+  end
+
+  # Test the new
+  test "view new book" do
+    flunk "Test not implemented yet"
+  end
+
+  # Test the destroy
+  test "destroy book" do
+    flunk "Test not implemented yet"
+  end
+
+  test "destroy book that doesnt exist" do
+    flunk "Test not implemented yet"
   end
 end
