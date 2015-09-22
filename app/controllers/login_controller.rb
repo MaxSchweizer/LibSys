4463e43a7d31f34a930e1c03dc8f17c7b241d19a
Max Schweizer <meschwei@ncsu.edu>
2015-09-22 13:40:12 -0400

    Added CanCanCan for authorization. Needs permissions set

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,4 +1,6 @@
 class LoginController < ApplicationController
+  
+  skip_load_and_authorize_resource
 
   def index
   end
---
+++ app/models/ability.rb
@@ -0,0 +1,17 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # See the wiki for details:
+    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+
+    # TODO: Put in the correct permissions here for who can see/do which actions of the controllers...
+
+    user ||= User.new # guest user (not logged in)
+    if user.type == "Admin"
+      can :read, :all
+    elsif user.type == "LibraryMember"
+      can :read, :all
+    end
+  end
+end
