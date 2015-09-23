3e6a0472cd276cafe77790d77fd8cc3051b367f0
ngohara <ngohara@ncsu.edu>
2015-09-23 16:10:05 -0400

    added admin edit admin to ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,6 +10,7 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.admin?
       can :read, :all
+      can [:edit], Admin
       can [:create], Admin
     elsif user.library_member?
       can [:show, :update], LibraryMember
