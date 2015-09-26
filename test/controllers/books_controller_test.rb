f241efc01e5a830b38973b71bb0cc9878a4c0d74
Max Schweizer <meschwei@ncsu.edu>
2015-09-25 23:12:19 -0400

    Fixed tests...

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -7,13 +7,14 @@ class BooksControllerTest < ActionController::TestCase
     user = LibraryMember.create!(:name => "n", :email => "n@g.com", :password => "123")
     session[:user_id] = user.id
   end
+
   # teardown session id so next test refreshes
   def teardown
     session[:user_id] = nil
     user = nil
   end
 
-#===== test search features =============================================================================================================
+  #===== test search features =============================================================================================================
   test "should be able to search by authors with exact string" do
     get :index, {"search_type" => "authors", "query" => "Author0"}
     assert_response :success
@@ -100,7 +101,7 @@ class BooksControllerTest < ActionController::TestCase
     assert (assigns(:books).length == 12), "Incorrect number of books returned. Check fixtures for details."
   end
 
-#=======test other methods ===============================================================================================
+  #=======test other methods ===============================================================================================
   # Test the index
   test "should get index" do
     get :index
@@ -111,9 +112,9 @@ class BooksControllerTest < ActionController::TestCase
     # TODO: Write more unit test for this.  Perhaps decrease the number of books in the fixture for comprehensive testing.
   end
 
-   # Test the create
+  # Test the create
   test "create new book fail as librarymember " do
-    post :create, book: {isbn: "200", title: "200", authors: "200", description: "200" } #, status: "false"}
+    post :create, book: {isbn: "200", title: "200", authors: "200", description: "200"} #, status: "false"}
     assert_response :redirect
     #assert_redirected_to book_path(assigns(@book))
   end
@@ -123,7 +124,7 @@ class BooksControllerTest < ActionController::TestCase
     user = Admin.create!(:name => "n", :email => "n@g.com", :password => "123")
     lib_user = LibraryMember.create!(:name => "n2", :email => "n2@g.com", :password => "123")
     session[:user_id] = user.id
-    post :create, { book: {isbn: "200", title: "200", authors: "200", description: "200" } , status: "false",  library_member:lib_user.id }
+    post :create, {book: {isbn: "200", title: "200", authors: "200", description: "200"}, status: "false", library_member: lib_user.id}
     assert_response :redirect
     assert_redirected_to Book.find_by(title: "200"), :action => "show"
     assert "200", Book.find_by(title: "200").authors
@@ -134,8 +135,9 @@ class BooksControllerTest < ActionController::TestCase
     user = Admin.create!(:name => "n", :email => "n@g.com", :password => "123")
     lib_user = LibraryMember.create!(:name => "n2", :email => "n2@g.com", :password => "123")
     session[:user_id] = user.id
-    post :create, { book: {isbn: "0101", title: "200", authors: "200", description: "200" } , status: "false",  library_member:lib_user.id }
-    assert_response :redirect
+    post :create, {book: {isbn: "0101", title: "This is the changed title", authors: "200", description: "200"}, status: "false", library_member: lib_user.id}
+    assert_response :success
+    assert_equal "Yellow", Book.find_by_isbn("0101").title
   end
 
 
